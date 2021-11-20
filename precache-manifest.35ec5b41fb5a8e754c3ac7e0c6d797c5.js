578fe1ac7428c98119eea9a2dd5cd4107a5555bc
arahangua <arahangua@gmail.com>
2021-11-20 22:20:56 +0900

    Updates

---
+++ precache-manifest.35ec5b41fb5a8e754c3ac7e0c6d797c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ed3e4bcb1f1fa2d8d5be2568c3a21d",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "0ab86a4cfb44642a8c64",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.4e957331.chunk.css"
+  },
+  {
+    "revision": "6148a53333f5eca3e8ca",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.247b9a16.chunk.js"
+  },
+  {
+    "revision": "002ec1fb75b480b97330",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/3.f5cc348b.chunk.js"
+  },
+  {
+    "revision": "59a606cdfa5d5b04dad2",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/4.68ec6be1.chunk.js"
+  },
+  {
+    "revision": "4065d69f6222e0752b8b",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/5.8f6e71ed.chunk.js"
+  },
+  {
+    "revision": "0ab86a4cfb44642a8c64",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.b58c8bd6.chunk.js"
+  },
+  {
+    "revision": "972d96da3970014f0bb4",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.7e521392.js"
+  }
+]);
